36a35c6a9e37a9ce65d6bb1e6098652671018a90
InvestHor <p.kulich@yahoo.com>
2020-02-12 20:45:11 +0100

    sos filters

--- filter.py
+++ filter.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.signal import butter, lfilter
+from scipy.signal import butter, lfilter, sosfilt
 import numpy as np
 
 
 def import_file(directory, file):
     path = f'{directory}/{file}'
-    data_frame = pd.read_fwf(path).dropna()
+    data_frame = pd.read_fwf(path).dropna().reset_index()
     return data_frame
 
 
@@ -50,6 +50,39 @@ def butter_highpass_filter(data, highcut, fs, order=5):
     return y
 
 
+def butter_filter_plot():
+    # b, a = _butter_lowpass(40, 1000, 5)
+    # # b, a = butter(4, 40, 'low', analog=True)
+    # w, h = freqs(b, a)
+    # plt.semilogx(w, 20 * np.log10(abs(h)))
+    # plt.title('Butterworth filter frequency response')
+    # plt.xlabel('Frequency [radians / second]')
+    # plt.ylabel('Amplitude [dB]')
+    # plt.margins(0, 0.1)
+    # plt.grid(which='both', axis='both')
+    # plt.axvline(40, color='green')  # cutoff frequency
+    # plt.show()
+    pass
+
+
+def sos_butter_lowpass(data, lowcut, fs, order=10):
+    sos = butter(order, lowcut, 'lp', fs=fs, output='sos')
+    filtered = sosfilt(sos, data)
+    return filtered
+
+
+def sos_butter_highpass(data, highcut, fs, order=10):
+    sos = butter(order, highcut, 'hp', fs=fs, output='sos')
+    filtered = sosfilt(sos, data)
+    return filtered
+
+
+def sos_butter_bandpass(data, highcut, lowcut, fs, order=10):
+    sos = butter(order, [highcut, lowcut], 'bp', fs=fs, output='sos')
+    filtered = sosfilt(sos, data)
+    return filtered
+
+
 def plot_frequency(x, filtered, not_filtered, N):
     plt.plot(x[0:N // 2], np.abs(filtered)[0:N // 2] / N)
     plt.plot(x[0:N // 2], np.abs(not_filtered)[0:N // 2] / N)
@@ -62,7 +95,7 @@ def plot_frequency(x, filtered, not_filtered, N):
 
 
 def plot_wavelength(x, y, N):
-    plt.bar(N * x[2:N // 2] ** (-1), np.abs(y)[2:N // 2] / N)
+    plt.plot(N * x[2:N // 2] ** (-1), np.abs(y)[2:N // 2] / N)
     plt.xlabel('wavelength [m]')
     plt.ylabel('amplitude [mm]')
     plt.xscale('log')
@@ -70,50 +103,3 @@ def plot_wavelength(x, y, N):
     plt.grid(True, which='minor')
     plt.savefig('plots/dft.png')
     plt.show()
-
-
-def main():
-    # data import - you need to set propper arguments in import_file function
-    data = import_file("data", "EXPORT01.txt")
-    magnitude = data["AlgPrL"]
-    x = data['_KM.M____']
-
-    # input data
-    fs = 1000
-    low_f = 333
-    high_f = 40
-    ############
-
-    # low pass filter for non filtered data
-    filtered_data_low = butter_lowpass_filter(magnitude, low_f, fs, 5)
-
-    # high pass filter for non filtered data
-    filtered_data_high = butter_highpass_filter(magnitude, high_f, fs, 5)
-
-    # high pass filter for non filtered data
-    filtered_data_band = butter_bandpass_filter(magnitude, low_f, high_f, fs, 5)
-
-    # plot all
-    plt.plot(x, magnitude)
-    plt.plot(x, filtered_data_low)
-    plt.plot(x, filtered_data_high)
-    plt.plot(x, filtered_data_band)
-    plt.legend(('original', 'lowpass', 'highpass', 'bandpass'))
-    plt.grid(True, which='major')
-    plt.grid(True, which='minor')
-    plt.show()
-
-    # fft calculation
-    amp = np.fft.fft(magnitude)
-    amp_band = np.fft.fft(filtered_data_high)
-
-    # plot
-    N = len(magnitude)
-    T = x[1] - x[0]
-    f = np.linspace(0, 1 / T, N)
-    plot_frequency(f, amp_band, amp, N)
-    plot_wavelength(f, amp_band, N)
-
-
-if __name__ == '__main__':
-    main()
